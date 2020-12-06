3ea5e698ae5cb8ca425305df009d94ed52c8aa5c
Yauhen Chaika <ychaika@slb.com>
2020-12-06 21:40:05 +0300

    feat: change cart URL to HTTPS proxy

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -4,7 +4,7 @@ const API_PATHS = {
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://fa7hugwpbk.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://xdom87cc4d.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://eugenech-cart-api-dev.eu-west-1.elasticbeanstalk.com/api'
+  cart: 'https://qb9q0eg7h5.execute-api.eu-west-1.amazonaws.com/dev/api'
 };
 
 export default API_PATHS;
